e41269f24a8c97da03634daba220c541d7a53059
Alex Eastman <alexeast@buffalo.edu>
2019-12-29 21:48:36 -0500

    Added enhanced functionality to moving pieces. Board class now tracks where pieces are on the board as well as valid moves. A valid move method was added to confirm or deny a players move.

--- model/board.js
+++ model/board.js
@@ -1,14 +1,16 @@
 class board {
-    gameBoard = {
-        "A": ["0", "2", "4", "6"],
-        "B": ["1", "3", "5", "7"],
-        "C": ["0", "2", "4", "6"],
-        "D": ["1", "3", "5", "7"],
-        "E": ["0", "2", "4", "6"],
-        "F": ["1", "3", "5", "7"],
-        "G": ["0", "2", "4", "6"],
-        "H": ["1", "3", "5", "7"]
-    };
+
+    //valid squares on the board
+    gameBoard = [
+        "A0", "A2", "A4", "A6",
+        "B1", "B3", "B5", "B7",
+        "C0", "C2", "C4", "C6",
+        "D1", "D3", "D5", "D7",
+        "E0", "E2", "E4", "E6",
+        "F1", "F3", "F5", "F7",
+        "G0", "G2", "G4", "G6",
+        "H1", "H3", "H5", "H7"
+    ];
 
     //starting positions
     black = [
@@ -23,18 +25,32 @@ class board {
         "C0", "C2", "C4", "C6"
     ];
 
+    validMove(piece, prv, nxt) {
+        if (this.gameBoard.indexOf(nxt) < 0 || this.black.indexOf(nxt) >= 0 || this.red.indexOf(nxt) >= 0) return false;
+
+        if (piece.king) {
+            return true;
+        } else if (piece.color === "red" && prv[0] < nxt[0]) {
+            return true;
+        } else return piece.color === "black" && prv[0] > nxt[0];
+
+    }
+
     pieces = [];
 }
 
 class piece {
     constructor(color, position, id) {
-        this.color = "assets/images/decorative_" + color + ".svg";
+        this.color = color;
+        this.src = "assets/images/decorative_" + color + ".svg";
         this.position = position;
         this.id = id;
-        this.html = "<img id='" + this.id + "' src=" + this.color + " style='height: 100%;'>";
+        this.html = "<img id='" + this.id + "' src=" + this.src + " style='height: 100%;'>";
     }
+    src = "";
     color = "";
     position = "";
     html = "";
     id = 0;
+    king = false;
 }
--- view/js/game.js
+++ view/js/game.js
@@ -19,16 +19,31 @@ function newGame() {
 function addPiece(piece, position) {
     $("#" + position).append(piece.html);
     piece.position = position;
+    if (piece.color === "black") {
+        game.black.push(piece.position);
+    } else {
+        game.red.push(piece.position);
+    }
 }
 
-function removePiece(position) {
-    $("#" + position).remove("img");
+function removePiece(piece, position) {
+    $("#" + position).children("img").remove();
+    if (piece.color === "black") {
+        game.black.splice(game.black.indexOf(position), 1);
+    } else {
+        game.black.splice(game.red.indexOf(position), 1);
+    }
 }
 
 function move(piece, prv, nxt) {
-    removePiece(prv);
-    addPiece(piece, nxt);
+    if (game.validMove(piece, prv, nxt)) {
+        console.log("valid move");
+        removePiece(piece, prv);
+        addPiece(piece, nxt);
+    }
 }
+
+let temp;
 
 $(document).ready(function() {
     let height = $(window).height();
@@ -36,7 +51,13 @@ $(document).ready(function() {
 
     $(".board-col").width(sq).height(sq).click(function() {
         let id = $(this).children("img").attr("id");
+        let pos = $(this).attr("id");
+        console.log(pos);
         let piece = game.pieces[id];
+        if (!temp) {
+            temp = piece;
+        }
+        else move(temp, temp.position, pos);
     });
 
     newGame();
