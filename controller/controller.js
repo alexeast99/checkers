32d5c9e88ce6d424c4d1370c6e29beffa79732eb
Alex Eastman <alexeast@buffalo.edu>
2020-01-01 14:30:40 -0500

    Removed unneeded controller directory and model.txt file.

--- controller/controller.js
+++
@@ -1,3 +0,0 @@
-/* Controller must take information about game state from model and pass it to view.
- * Controller will disseminate what needs to be done based on game state from model and pass relevant information to view.
-*/
